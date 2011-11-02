29829bfc8cbfb3d00fe00bdbf67eddfe837704df
Jonathan Mumm <mumm@tokbox.com>
2011-11-02 15:20:50 -0700

    Set up main page and user authentication

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'omniauth'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, "BzT8BAHK9N81hM1pj18kw", "M1Z2wW0Z2O0webyYmYDzq04sljzJa0p4WTZCtSsLVY"
+end
