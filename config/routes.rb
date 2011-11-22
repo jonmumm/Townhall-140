e91efd49fc791be26cfc2159ba047e178b287ca3
Jonathan Mumm <mumm@tokbox.com>
2011-11-21 18:16:09 -0800

    updated route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ TownHall140::Application.routes.draw do
   get "event" => "event#index"
   get "event/current"
 
-  root :to => "main#index"
+  root :to => redirect("/shows/20")
 
   get "shows/new" => "shows#new", :as => :new_show
   get "shows/:id/admin" => "shows#admin", :as => :show_admin
