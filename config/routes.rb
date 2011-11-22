4f37c67cbe9e7ee9535c2a9c72bbe285ca75e0cb
Jonathan Mumm <mumm@tokbox.com>
2011-11-21 18:11:35 -0800

    updated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 TownHall140::Application.routes.draw do
+
+  resource :shows
+  match "shows/:id/admin" => "shows#admin", :as => :show_admin
+  
   post "event" => "event#create"
   get "event" => "event#index"
   get "event/current"
 
-  root :to => "main#index"
-
-  get "shows/new" => "shows#new", :as => :new_show
-  get "shows/:id/admin" => "shows#admin", :as => :show_admin
-  get "shows/:id" => "shows#show", :as => :show
-  post "shows" => "shows#create"
+  root :to => redirect("/shows/20")
 
   get "auth/:provider/callback" => "sessions#create"
   get "signout" => "sessions#destroy", :as => :signout
