ab1f3c3eb9e8a44308068a257ca63520a7fe57a1
Jonathan Mumm <mumm@tokbox.com>
2011-11-23 11:44:02 -0800

    remoed assets, updated sass in gem

--- public/assets/backbone/models/App-9dbe36b30e61d2be9bc334fd6eb634fa.js
+++
@@ -1,1 +0,0 @@
-((function(){TownHall140.Models.App=Backbone.Model.extend({setup:function(){return this.set({participants:new TownHall140.Collections.ParticipantCollection,events:new TownHall140.Collections.EventManager})},start:function(){return this.get("session").connect(this.get("opentok_api_key"),this.get("opentok_token"))}})})).call(this)
