964d582f0a956aef8847e913c5c20edd01685274
Jonathan Mumm <mumm@tokbox.com>
2011-11-23 15:06:03 -0800

    Heroku env settings

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => 'jonmumm',
-  :password => 'th140!',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
