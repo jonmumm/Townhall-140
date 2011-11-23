c5e2813f74cee18a46d28ec60ef15dba8c5cd31d
Jonathan Mumm <mumm@tokbox.com>
2011-11-23 13:45:01 -0800

    Added mailto link in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ TownHall140::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'townhall140.com' }
 end
