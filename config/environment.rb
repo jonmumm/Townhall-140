9a4d6a054ef98db8b1448e0895aeffb0f5c70852
Jonathan Mumm <mumm@tokbox.com>
2011-11-23 15:02:46 -0800

    Added environment smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,13 @@ require File.expand_path('../application', __FILE__)
 TownHall140::Application.initialize!
 
 Time::DATE_FORMATS[:start_time_format] = "%A, %B %d at %l:%M%p %Z"
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => 'jonmumm',
+  :password => 'th140!',
+  :domain => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
