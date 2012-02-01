28f0f24666f9f9936b18ace008da0920d6f1beae
Daniel Choi <dhchoi@gmail.com>
2012-02-01 08:40:57 -0500

    Controller spec for voting up question logged in

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,20 +14,32 @@ describe QuestionsController do
   end
 
   it "should let anonymous user create a new question and record the remote ip" do
-    expect { 
-      post :create, {show_id: 1, body: 'hello'}
-    }.to change(Question, :count).by(1) 
+    expect { post :create, {show_id: 1, body: 'hello'} }.to change(Question, :count).by(1) 
     Question.last.ip.should_not be nil
     Question.last.ip.should match /\d+.\d+.\d+.\d+/
   end
 
   it "should allow anonymous users to vote up a question and record the remote ip" do
-    expect {
-      post :vote_up, id: @question.id
-    }.to change(Vote, :count).by(1)
+    expect { post :vote_up, id: @question.id }.to change(Vote, :count).by(1)
     Vote.last.ip.should_not be nil
     Vote.last.ip.should match /\d+.\d+.\d+.\d+/
   end
+
+  it "should let logged user create a new question and record the remote ip and user_id" do
+    sign_in (@user = Factory.create(:user))
+    expect { post :create, {show_id: 1, body: 'hello'} }.to change(Question, :count).by(1) 
+    Question.last.ip.should match /\d+.\d+.\d+.\d+/
+    Question.last.user.should == @user
+  end
+
+  it "should allow logged in user to vote up a question and record the remote ip and user_id" do
+    sign_in (@user = Factory.create(:user))
+    expect { post :vote_up, id: @question.id }.to change(Vote, :count).by(1)
+    Vote.last.ip.should match /\d+.\d+.\d+.\d+/
+    Vote.last.user.should == @user
+  end
+
+
 
   it "create action should redirect when model is valid" do
     pending
